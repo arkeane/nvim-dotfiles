fcc642688e131b6a5f13e3f97a381d58c4f40768
Ludovico Pestarino <ludovico@pestarino.xyz>
2024-02-11 17:15:47 +0100

    Added Undotree

---
+++ lua/plugins/undo.lua
@@ -0,0 +1,5 @@
+local Plugin = {'mbbill/undotree'}
+
+Plugin.event = 'VeryLazy'
+
+return Plugin
--- lua/remap.lua
+++ lua/remap.lua
@@ -5,5 +5,8 @@ vim.keymap.set('n', '<leader>fg', telescope.live_grep, {})
 vim.keymap.set('n', '<leader>fb', telescope.buffers, {})
 vim.keymap.set('n', '<leader>fh', telescope.help_tags, {})
 
+-- Undotree remaps
+vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+
 -- Shortcuts
 vim.keymap.set("x", "<leader>p", [["_dP]])
