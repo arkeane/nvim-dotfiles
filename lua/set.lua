e5b7075b3cc9e269617e30d5de9d110578b64ebc
Ludovico Pestarino <ludovico@pestarino.xyz>
2024-02-11 17:01:51 +0100

    Added useful settings

--- lua/set.lua
+++ lua/set.lua
@@ -2,6 +2,7 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 vim.opt.termguicolors = true
+vim.opt.guicursor = ""
 
 vim.opt.number = true
 vim.opt.relativenumber = true
@@ -14,6 +15,7 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
+vim.opt.smartindent = true
 
 vim.opt.wrap = false
 vim.opt.breakindent = true
@@ -24,6 +26,13 @@ vim.opt.incsearch = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.showmatch = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.local/share/nvim"
+vim.opt.undofile = true
+
+vim.opt.signcolumn = "yes"
 
 vim.opt.scrolloff = 8
 
