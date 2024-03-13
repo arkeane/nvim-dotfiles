2688e355adaf1f39ed2c398a1886be5b3d9e7e4e
arkeane <ludovico@pestarino.xyz>
2024-03-13 15:02:21 +0100

    added rust treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,7 +7,7 @@ Plugin.config = function()
 	local configs = require("nvim-treesitter.configs")
 
 	configs.setup({
-		ensure_installed = { "c", "lua" },
+		ensure_installed = { "c", "lua", "rust" },
 		sync_install = false,
 		highlight = { enable = true },
 		indent = { enable = true },
