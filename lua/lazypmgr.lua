f6592f9ca90504df99a0e5bf1bd6f9698190d6f9
arkeane <ludovico@pestarino.xyz>
2024-08-11 10:12:09 +0200

    moved theme setting

--- lua/lazypmgr.lua
+++ lua/lazypmgr.lua
@@ -14,5 +14,3 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
 	{ import = "plugins" },
 })
-
-vim.cmd([[colorscheme dracula]])
--- lua/plugins/dracula.lua
+++ lua/plugins/dracula.lua
@@ -2,4 +2,8 @@ local Plugin = { "Mofiqul/dracula.nvim" }
 
 Plugin.event = "VimEnter"
 
+Plugin.config = function()
+    vim.cmd("colorscheme dracula")  -- Apply the theme
+end
+
 return Plugin
