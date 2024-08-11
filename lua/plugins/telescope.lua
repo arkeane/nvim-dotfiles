ba9a7aa21d963859b1c1770dea2291812202c9b5
arkeane <ludovico@pestarino.xyz>
2024-08-11 09:32:26 +0200

    Added hidden files search

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,9 +1,21 @@
 local Plugin = { "nvim-telescope/telescope.nvim" }
 
-Plugin.event = "VeryLazy"
-
 Plugin.dependencies = {
 	{ "nvim-lua/plenary.nvim" },
 }
 
+Plugin.cmd = "Telescope"
+
+Plugin.config = function()
+    local telescope = require("telescope")
+    telescope.setup({
+        pickers = {
+            find_files = {
+                hidden = true
+            }
+        }
+    })
+end
+
 return Plugin
+
