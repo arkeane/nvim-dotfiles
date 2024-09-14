16a36562f42c674ad53674b32499cdf710d02fac
Ludovico pestarino <ludovico@pestarino.xyz>
2024-09-14 11:00:59 +0200

    improvements

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -1,87 +1,88 @@
 local Plugin = { "neovim/nvim-lspconfig" }
 
 Plugin.dependencies = {
-	"williamboman/mason.nvim",
-	"williamboman/mason-lspconfig.nvim",
-	"hrsh7th/cmp-nvim-lsp",
-	"hrsh7th/cmp-buffer",
-	"hrsh7th/cmp-path",
-	"hrsh7th/cmp-cmdline",
-	"hrsh7th/nvim-cmp",
-	"L3MON4D3/LuaSnip",
-	"saadparwaiz1/cmp_luasnip",
-	"j-hui/fidget.nvim",
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/nvim-cmp",
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
+    "j-hui/fidget.nvim",
+    "folke/neodev.nvim", -- Add this for better Lua development
 }
 
 Plugin.config = function()
-	local cmp = require("cmp")
-	local cmp_lsp = require("cmp_nvim_lsp")
-	local capabilities =
-		vim.tbl_deep_extend("force", {}, vim.lsp.protocol.make_client_capabilities(), cmp_lsp.default_capabilities())
+    local cmp = require("cmp")
+    local cmp_lsp = require("cmp_nvim_lsp")
+    local capabilities = vim.tbl_deep_extend("force", {}, vim.lsp.protocol.make_client_capabilities(),
+        cmp_lsp.default_capabilities())
 
-	require("fidget").setup({})
-	require("mason").setup()
-	require("mason-lspconfig").setup({
-		ensure_installed = {
-			"lua_ls"
-		},
-		handlers = {
-			function(server_name)
-				require("lspconfig")[server_name].setup({
-					capabilities = capabilities,
-				})
-			end,
+    require("fidget").setup({})
+    require("mason").setup()
+    require("neodev").setup() -- Setup neodev for better Lua development
 
-			["lua_ls"] = function()
-				local lspconfig = require("lspconfig")
-				lspconfig.lua_ls.setup({
-					capabilities = capabilities,
-					settings = {
-						Lua = {
-							diagnostics = {
-								globals = { "vim", "it", "describe", "before_each", "after_each" },
-							},
-						},
-					},
-				})
-			end,
+    local on_attach = function(client, bufnr)
+        -- Enable completion triggered by <c-x><c-o>
+        vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-            ["pyright"] = function ()
-                local lspconfig = require("lspconfig")
-                lspconfig.pyright.setup({
-                    capabilities = capabilities
+        -- Key mappings
+        local opts = { noremap = true, silent = true, buffer = bufnr }
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+        vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+
+        -- Format on save
+        if client.server_capabilities.documentFormattingProvider then
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = vim.api.nvim_create_augroup("Format", { clear = true }),
+                buffer = bufnr,
+                callback = function() vim.lsp.buf.format() end
+            })
+        end
+    end
+
+    require("mason-lspconfig").setup({
+        ensure_installed = {
+            "lua_ls",
+            "pyright",
+            "rust_analyzer",
+        },
+        handlers = {
+            function(server_name)
+                require("lspconfig")[server_name].setup({
+                    capabilities = capabilities,
+                    on_attach = on_attach,
+                })
+            end,
+
+            ["lua_ls"] = function()
+                require("lspconfig").lua_ls.setup({
+                    capabilities = capabilities,
+                    on_attach = on_attach,
+                    settings = {
+                        Lua = {
+                            diagnostics = {
+                                globals = { "vim", "it", "describe", "before_each", "after_each" },
+                            },
+                            workspace = {
+                                checkThirdParty = false,
+                            },
+                        },
+                    },
                 })
             end,
 
             ["rust_analyzer"] = function()
-                local lspconfig = require("lspconfig")
-                local on_attach = function(client, bufnr)
-                    -- Enable completion triggered by <c-x><c-o>
-                    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-                    -- Key mappings
-                    local opts = { noremap=true, silent=true, buffer=bufnr }
-                    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-                    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-                    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-                    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-                    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-                    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-                    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-                    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
-                    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-
-                    -- Format on save
-                    if client.server_capabilities.documentFormattingProvider then
-                        vim.api.nvim_create_autocmd("BufWritePre", {
-                            group = vim.api.nvim_create_augroup("Format", { clear = true }),
-                            buffer = bufnr,
-                            callback = function() vim.lsp.buf.format() end
-                        })
-                    end
-                end
-
-                lspconfig.rust_analyzer.setup({
+                require("lspconfig").rust_analyzer.setup({
                     capabilities = capabilities,
                     on_attach = on_attach,
                     settings = {
@@ -121,29 +122,30 @@ Plugin.config = function()
                     },
                 })
             end
-		},
-	})
+        },
+    })
 
-	local cmp_select = { behavior = cmp.SelectBehavior.Select }
+    local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
-	cmp.setup({
-		snippet = {
-			expand = function(args)
-				require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-			end,
-		},
-		mapping = cmp.mapping.preset.insert({
-			["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-			["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-			["<CR>"] = cmp.mapping.confirm({ select = true }),
-			["<C-Space>"] = cmp.mapping.complete(),
-		}),
-		sources = cmp.config.sources({
-			{ name = "nvim_lsp" },
-			{ name = "luasnip" },
-			{ name = "buffer" },
-		}),
-	})
+    cmp.setup({
+        snippet = {
+            expand = function(args)
+                require("luasnip").lsp_expand(args.body)
+            end,
+        },
+        mapping = cmp.mapping.preset.insert({
+            ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+            ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+            ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+            ['<C-Space>'] = cmp.mapping.complete(),
+        }),
+        sources = cmp.config.sources({
+            { name = 'nvim_lsp' },
+            { name = 'luasnip' },
+            { name = 'buffer' },
+            { name = 'path' },
+        }),
+    })
 end
 
 return Plugin
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,17 +1,82 @@
 local Plugin = { "nvim-treesitter/nvim-treesitter" }
 
-Plugin.event = "VeryLazy"
+Plugin.event = { "BufReadPost", "BufNewFile" }
 Plugin.build = ":TSUpdate"
 
-Plugin.config = function()
-	local configs = require("nvim-treesitter.configs")
+Plugin.dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    "windwp/nvim-ts-autotag",
+    "JoosepAlviste/nvim-ts-context-commentstring",
+}
 
-	configs.setup({
-		ensure_installed = { "c", "lua", "rust", "python" },
-		sync_install = false,
-		highlight = { enable = true },
-		indent = { enable = true },
-	})
+Plugin.config = function()
+    -- Set up ts_context_commentstring
+    require('ts_context_commentstring').setup {}
+    vim.g.skip_ts_context_commentstring_module = true
+
+    require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+            "lua", "vim", "vimdoc", "query",           -- Neovim-related
+            "rust", "python", "c", "cpp",              -- Common programming languages
+            "javascript", "typescript", "html", "css", -- Web development
+            "json", "yaml", "toml",                    -- Data formats
+            "markdown", "markdown_inline",             -- Documentation
+            "regex",                                   -- Useful for many languages
+        },
+        sync_install = false,
+        auto_install = true,
+        ignore_install = {},
+        modules = {},
+        highlight = {
+            enable = true,
+            additional_vim_regex_highlighting = false,
+        },
+        indent = { enable = true },
+        incremental_selection = {
+            enable = true,
+            keymaps = {
+                init_selection = "<C-space>",
+                node_incremental = "<C-space>",
+                scope_incremental = "<C-s>",
+                node_decremental = "<C-backspace>",
+            },
+        },
+        textobjects = {
+            select = {
+                enable = true,
+                lookahead = true,
+                keymaps = {
+                    ["af"] = "@function.outer",
+                    ["if"] = "@function.inner",
+                    ["ac"] = "@class.outer",
+                    ["ic"] = "@class.inner",
+                },
+            },
+            move = {
+                enable = true,
+                set_jumps = true,
+                goto_next_start = {
+                    ["]m"] = "@function.outer",
+                    ["]]"] = "@class.outer",
+                },
+                goto_next_end = {
+                    ["]M"] = "@function.outer",
+                    ["]["] = "@class.outer",
+                },
+                goto_previous_start = {
+                    ["[m"] = "@function.outer",
+                    ["[["] = "@class.outer",
+                },
+                goto_previous_end = {
+                    ["[M"] = "@function.outer",
+                    ["[]"] = "@class.outer",
+                },
+            },
+        },
+        autotag = {
+            enable = true,
+        },
+    })
 end
 
 return Plugin
