b9ea190ec106b35e11175249f4b775505e245bce
Ludovico Pestarino <ludovico@pestarino.xyz>
2025-07-03 21:21:44 +0200

    added transparent background

--- lua/plugins/dracula.lua
+++ lua/plugins/dracula.lua
@@ -3,6 +3,11 @@ local Plugin = { "Mofiqul/dracula.nvim" }
 Plugin.event = "VimEnter"
 
 Plugin.config = function()
+    local dracula = require("dracula")
+    dracula.setup({
+        transparent_bg = true,
+    })
+
     vim.cmd("colorscheme dracula")  -- Apply the theme
 end
 
